e643268f483289d4b0afd3703e54126cadc49cdb
Konstantin Ullrich <konstantinullrich12@gmail.com>
2025-09-04 15:50:15 +0200

    feat: add initial Bluetooth support and refactor method call handling logic

---
+++ ios/Classes/MethodCallOperation.swift
@@ -0,0 +1,11 @@
+//
+//  MethodCallOperation.swift
+//  
+//
+//  Created by Konstantin Ullrich on 04.09.25.
+//
+
+
+class MethodCallOperation {
+    func onMethodCall(methodCall: FlutterMethodCall, result: FlutterResult) {}
+}
--- ios/Classes/MethodCallRegistry.swift
+++ ios/Classes/MethodCallRegistry.swift
@@ -1,19 +1,4 @@
 import Flutter
-import UIKit
-
-public class BitboxFlutterPlugin: NSObject, FlutterPlugin {
-    private let registry = MethodCallRegistry()
-    
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "bitbox_usb", binaryMessenger: registrar.messenger())
-    let instance = BitboxFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-      registry.onMethodCall(call: call, result: result)
-  }
-}
 
 class MethodCallRegistry {
     private var operations: [String: MethodCallOperation] = [:]
